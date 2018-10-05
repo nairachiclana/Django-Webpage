418854dd0373d7fd1c120257b9dfe9fa5b145bfe
nairachiclana <nairachiclana@gmail.com>
2018-10-05 15:25:24 +0000

    settings modified

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w9i)o6+cc3kn*-dvqniunl6tul-jcx5k7zob@cd!r)#tirmu@j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nairachiclana.pythonanywhere.com']
 
 
 # Application definition
@@ -117,6 +117,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
